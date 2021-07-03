1766428abc53dbf4234577f82cd76a625cc93ba0
Shreyas S Kumar <shreyassreekripa@gmail.com>
2021-07-03 11:56:21 +0530

    frontend initial stage

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const app = express();
 const port = process.env.PORT || 8080;
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "/index.html"));
+  console.log(__dirname);
 });
-
+app.use('/css',express.static(path.join(__dirname, 'public/css')));
 app.listen(port);
 console.log("Server started at http://localhost:" + port);
