bf112d6a69fbb2a18b377e546769126708044588
Shemin Anto <13395437+sheminanto@users.noreply.github.com>
2021-07-06 15:45:33 +0530

    for heroku

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const { socketServer } = require("./socketserver");
 socketServer();
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8000;
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "/index.html")); 
+  res.sendFile(path.join(__dirname, "/index.html"));
   console.log(__dirname);
 });
-app.use('/css',express.static(path.join(__dirname, 'public/css')));
-app.use('/js',express.static(path.join(__dirname, 'public/javascripts')));
-app.use('/sounds',express.static(path.join(__dirname, 'public/sounds')));
-app.listen(port,"0.0.0.0");
+app.use("/css", express.static(path.join(__dirname, "public/css")));
+app.use("/js", express.static(path.join(__dirname, "public/javascripts")));
+app.use("/sounds", express.static(path.join(__dirname, "public/sounds")));
+app.listen(port, "0.0.0.0");
 console.log("Server started at http://localhost:" + port);
